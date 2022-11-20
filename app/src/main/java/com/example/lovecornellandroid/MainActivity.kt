7802d3822ed9640c5ab7506a7f05cd4b7894eaa3
Claire4923 <zhouxiaoheclaire@163.com>
2022-11-19 20:01:26 -0500

    fragments and cell activity framework

--- app/src/main/java/com/example/lovecornellandroid/MainActivity.kt
+++ app/src/main/java/com/example/lovecornellandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lovecornellandroid
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,49 +11,45 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.lovecornellandroid.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.forum_item -> {
+                    val forumFragment = ForumFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, forumFragment)
+                        .commit()
+                }
+                R.id.post_item -> {
+                    val postFragment = PostFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, postFragment)
+                        .commit()
+                }
+                R.id.letters_item -> {
+                    val myLettersFragment = MyLettersFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, myLettersFragment)
+                        .commit()
+                }
+                else -> {
+                    Log.e("Unexpected", "Unexpected" + it.itemId)
+                }
+            }
+            true
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
